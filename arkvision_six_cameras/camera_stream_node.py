90a4bc483f556accc280f63bc40c7eafaa1a2a6e
Gunreben <gunreben@wifa.uni-leipzig.de>
2024-02-21 16:43:09 +0100

    adapted topic names again!

--- arkvision_six_cameras/camera_stream_node.py
+++ arkvision_six_cameras/camera_stream_node.py
@@ -12,8 +12,13 @@ class CameraStreamNode(Node):
         self.declare_parameter('camera_ip', '192.168.26.70')  # Declare camera_ip parameter with default value
         self.publish_rate = self.get_parameter('publish_rate').get_parameter_value().double_value
         camera_ip = self.get_parameter('camera_ip').get_parameter_value().string_value  # Get the camera_ip parameter
+	
+	parts = ip_address.split('.')
+    	# Get the last part of the IP address
+    	last_part = parts[-1]
+    
 
-        self.publisher_ = self.create_publisher(Image, 'camera_image/' + camera_ip, 10)
+        self.publisher_ = self.create_publisher(Image, 'camera_image/' + str(last_part), 10)
         self.bridge = CvBridge()
         self.timer = self.create_timer(1.0 / self.publish_rate, self.timer_callback)
         self.cap = cv2.VideoCapture(f"rtsp://{camera_ip}:8554/h264")  # Use the camera_ip parameter
