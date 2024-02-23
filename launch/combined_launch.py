ce54ce57615a50ad06d6834b30c5e4a66c8d306b
Gunreben <gunreben@wifa.uni-leipzig.de>
2024-02-23 15:21:06 +0100

    reworking combined launch file

--- launch/combined_launch.py
+++ launch/combined_launch.py
@@ -1,26 +1,39 @@
+from launch_ros.substitutions import FindPackageShare
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
     return LaunchDescription([
         # Include the blickfeld_qb2_ros2_driver launch file
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/blickfeld_qb2_ros2_driver/blickfeld_qb2_ros2_driver.launch.py']),
+                PythonLaunchDescriptionSource([
+                        PathJoinSubstitution([
+                                FindPackageShare("blickfeld_qb2_ros2_driver"),
+                                'launch',
+                                'blickfeld_qb2_ros2_driver.launch.py'
+                        ])
+                ])
         ),
-        
+
         # Run the arkvision_six_cameras node
         Node(
             package='arkvision_six_cameras',
             executable='camera_stream_node',
             name='camera_stream_node'
         ),
-        
+
         # Include the ros2_ouster driver launch file
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/ros2_ouster/driver_launch.py']),
-        ),
+                PythonLaunchDescriptionSource([
+                        PathJoinSubstitution([
+                                FindPackageShare('ros2_ouster'),
+                                'launch',
+                                'driver_launch.py'
+                        ])
+                ])
+        )
     ])
-
