01fa3771ac2e3a126c3574ba3dd7ccc7fb1c83c3
Gunreben <gunreben@wifa.uni-leipzig.de>
2024-03-18 10:02:25 +0100

    added hostname to ouster launchfile

--- launch/combined_launch.py
+++ launch/combined_launch.py
@@ -1,13 +1,19 @@
 from launch_ros.substitutions import FindPackageShare
-
 from launch import LaunchDescription
-from launch_ros.actions import Node, ComposableNodeContainer
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+    # Declare the sensor_hostname argument
+    ouster_hostname_arg = DeclareLaunchArgument(
+        'ouster_hostname',
+        default_value='192.168.26.85',
+        description='Hostname or IP of the Ouster'
+    )
+
     blickfeld_container = ComposableNodeContainer(
         name="blickfeld_qb2_component",
         namespace="",
@@ -20,7 +26,7 @@ def generate_launch_description():
                 name="blickfeld_qb2_driver",
                 parameters=[
                     {
-                        "fqdn": "192.168.26.26",
+                        "fqdn": "qb2",
                         "frame_id": "lidar",
                         "point_cloud_topic": "/bf/points_raw",
                         "use_measurement_timestamp": False,
@@ -33,7 +39,22 @@ def generate_launch_description():
         output="screen",
     )
 
+    # Include the ros2_ouster driver launch file with sensor_hostname parameter
+    ouster_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('ros2_ouster'),
+                'launch',
+                'sensor.launch.xml'
+            ])
+        ]),
+        launch_arguments={
+            'sensor_hostname': LaunchConfiguration('sensor_hostname'),
+        }.items(),
+    )
+
     return LaunchDescription([
+        ouster_hostname_arg,
         blickfeld_container,
 
         # Run the arkvision_six_cameras node
@@ -43,15 +64,6 @@ def generate_launch_description():
             name='camera_stream_node'
         ),
 
-        # Include the ros2_ouster driver launch file
-        IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([
-                        PathJoinSubstitution([
-                                FindPackageShare('ros2_ouster'),
-                                'launch',
-                                'driver_launch.py'
-                        ])
-                ])
-        )
+        ouster_launch
     ])
 
