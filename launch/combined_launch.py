05bc2dec1fb6ae3bab621d546351d662e573eb0b
Gunreben <gunreben@wifa.uni-leipzig.de>
2024-03-21 16:26:48 +0100

    Added ZED 2i to combined_launch

--- launch/combined_launch.py
+++ launch/combined_launch.py
@@ -1,5 +1,5 @@
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch import LaunchDescription, LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 
@@ -8,16 +8,30 @@ def generate_launch_description():
     arkvision_six_cameras_pkg_prefix = FindPackageShare('arkvision_six_cameras').find('arkvision_six_cameras')
     blickfeld_qb2_ros2_driver_pkg_prefix = FindPackageShare('blickfeld_qb2_ros2_driver').find('blickfeld_qb2_ros2_driver')
     ouster_ros_pkg_prefix = FindPackageShare('ouster_ros').find('ouster_ros')
+    zed_pkg_prefix = FindPackageShare('zed_wrapper').find('zed_wrapper')
 
     # Define the path to the launch files
     arkvision_launch_file = PythonLaunchDescriptionSource([arkvision_six_cameras_pkg_prefix, '/launch/camera_stream_launch.py'])
     blickfeld_launch_file = PythonLaunchDescriptionSource([blickfeld_qb2_ros2_driver_pkg_prefix, '/launch/blickfeld_qb2_ros2_driver.launch.py'])
     ouster_launch_file = PythonLaunchDescriptionSource([ouster_ros_pkg_prefix, '/launch/driver.launch.py'])
+    zed_launch_file = PythonLaunchDescriptionSource([zed_pkg_prefix, '/launch/zed_camera.launch.py'])
+
+    # Declare the camera_model launch argument
+    camera_model_arg = DeclareLaunchArgument(
+        'camera_model',
+        default_value='zed2i',
+        description='Model of the ZED camera.'
+    )
 
     # Include the launch files
     return LaunchDescription([
+        camera_model_arg,  # Declare the argument before including the launch file
         IncludeLaunchDescription(arkvision_launch_file),
         IncludeLaunchDescription(blickfeld_launch_file),
-        IncludeLaunchDescription(ouster_launch_file)
+        IncludeLaunchDescription(ouster_launch_file),
+        IncludeLaunchDescription(
+            zed_launch_file,
+            launch_arguments={'camera_model': LaunchConfiguration('camera_model')}.items()  # Pass the argument to the launch file
+        )
     ])
 
